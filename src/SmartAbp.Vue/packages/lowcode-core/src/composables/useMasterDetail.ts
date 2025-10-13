aa893c9fdce476a1af804e302e994de9622298f9
SmartAbp Developer <dev@smartabp.com>
2025-10-13 21:56:23 +0800

    自动提交: 10/13/2025 21:56:23 - Git安全同步前的本地更改

--- src/SmartAbp.Vue/packages/lowcode-core/src/composables/useMasterDetail.ts
+++ src/SmartAbp.Vue/packages/lowcode-core/src/composables/useMasterDetail.ts
@@ -191,7 +191,7 @@ export function useMasterDetail<
         // 否则标记为删除，稍后批量删除
         // 注意：使用类型安全的复制而非any
         const toDelete = detailList.value[index] as TDetail
-        ; (deletedDetails.value as unknown as TDetail[]).push({ ...(toDelete as TDetail) })
+          ; (deletedDetails.value as unknown as TDetail[]).push({ ...(toDelete as TDetail) })
         detailList.value.splice(index, 1)
       }
 
@@ -233,8 +233,8 @@ export function useMasterDetail<
         if (index !== -1) {
           if (!detail._isNew) {
             // 使用类型安全的复制而非any
-            const toDelete = detailList.value[index] as TDetail
-            ; (deletedDetails.value as unknown as TDetail[]).push({ ...(toDelete as TDetail) })
+              const toDelete = detailList.value[index] as TDetail
+                ; (deletedDetails.value as unknown as TDetail[]).push({ ...(toDelete as TDetail) })
           }
           detailList.value.splice(index, 1)
         }
